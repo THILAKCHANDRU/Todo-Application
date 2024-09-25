63c3a0e1b90dddd58d80f0f79650b5ade87c35dd
THILAKCHANDRU <thilakhacker@gmail.com>
2024-09-26 00:03:00 +0530

    Started with index page and header

---
+++ front-end/src/app/header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <>
+      <h1 className="text-center text-5xl pt-5 font-sans">Task</h1>
+      <h1 className="text-center text-5xl">Manager</h1>
+    </>
+  );
+}
